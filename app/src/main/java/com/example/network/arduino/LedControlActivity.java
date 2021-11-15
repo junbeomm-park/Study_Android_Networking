723b5f6dada871f94185b893b56c68b7786d2c43
park junbeom <junbum0674@gmail.com>
2021-11-15 17:12:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/network/arduino/LedControlActivity.java
@@ -0,0 +1,16 @@
+package com.example.network.arduino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.network.R;
+
+public class LedControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_led_control);
+    }
+}
