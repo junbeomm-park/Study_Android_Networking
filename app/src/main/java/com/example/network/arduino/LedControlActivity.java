0dd485f6e0a53993d90899c1793a792e86522a3e
park junbeom <junbum0674@gmail.com>
2021-11-16 18:24:08 +0900

    21.11.16 Android MQTT

--- app/src/main/java/com/example/network/arduino/LedControlActivity.java
+++ app/src/main/java/com/example/network/arduino/LedControlActivity.java
@@ -3,8 +3,11 @@ package com.example.network.arduino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.network.R;
 
@@ -18,11 +21,15 @@ public class LedControlActivity extends AppCompatActivity {
     BufferedReader serverIn; // 서버에서 보내오는 메시지 읽기 위한 스트림
     PrintWriter serverOut; // 서버로 메시지를 보내기 위한 스트림
     Socket server;
+    Handler handler;
+    TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led_control);
+        view = findViewById(R.id.showdata);
         new LedThread().start();
+        handler = new Handler(Looper.myLooper());
     }
     //버튼을 누를때 호출되는 메소드
     public void send_msg(View view){
@@ -32,7 +39,7 @@ public class LedControlActivity extends AppCompatActivity {
                 String message = "";
                 if(view.getId()==R.id.led_on){
                     message = "led_on";
-                }else {
+                }else if(view.getId()==R.id.led_off){
                     message = "led_off";
                 }
                 serverOut.println(message); //서버로 메시지 보내기
@@ -55,9 +62,25 @@ public class LedControlActivity extends AppCompatActivity {
                             String msg;
                             try {
                                 msg = serverIn.readLine();
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        view.append(msg+"\n");
+                                    }
+                                });
                                 Log.d("network","서버로부터 수신된 메시지>>>"+msg);
+
                             } catch (IOException e) {
-                                e.printStackTrace();
+                                //서버에서 연결이 끊어지는 경우 사용자는 자원을 반납
+                                try {
+                                    serverIn.close();
+                                    serverOut.close();
+                                    server.close();
+
+                                } catch (IOException ioException) {
+                                    ioException.printStackTrace();
+                                }
+                                break; //서버에서 연결이 끊어지거나 오류가 발생되면 무한루프를 빠져나와 쓰레드를 종료할 수 있도록 처리
                             }
                         }
                     }
